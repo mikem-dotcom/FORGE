8dbd28af81d3c885e3f8f3b311da7031ce05aad4
mikem-dotcom <mike.m@inspirationtravel.com>
2026-05-03 19:26:50 -0700

    Update asset paths in service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'forge-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const ASSETS = ['/FORGE/', '/FORGE/index.html', '/FORGE/manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
